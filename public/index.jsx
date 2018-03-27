cc05ed3d8da7e386defcadb9d6ba172e7bcdddf6
heathercoraje <heathercoraje@gmail.com>
2018-03-27 12:58:35 +0900

    change fs and refactor onClickButton eventhandlers

--- public/index.jsx
+++ public/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './containers/App.jsx';
+import App from '../src/containers/App.jsx';
 
 ReactDOM.render(<App />, document.getElementById('root'));
