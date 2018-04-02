ba9626198f367160f07b41cfdd68f649a7c8e36b
heathercoraje <heathercoraje@gmail.com>
2018-04-03 00:04:21 +0900

    very much undone

--- src/components/Setting.jsx
+++ src/components/Setting.jsx
@@ -10,7 +10,7 @@ class Setting extends React.Component {
 		this.setState({
 			isModalOpen: true
 		});
-    this.props.onSettingOpen('Stop');
+		this.props.onSettingOpen('Stop');
 	};
 	closeModal = () => {
 		this.setState({
@@ -19,15 +19,15 @@ class Setting extends React.Component {
 	};
 	render() {
 		return (
-			<div className='wrapper-setting'>
-				<span className='setting-icon' onClick={this.openModal}><i className="fas fa-cog"></i></span>
-				<Modal isOpen={this.state.isModalOpen} onCloseClick={this.closeModal}>
-					<h1>Timer Setting</h1>
-					<p>Choose your timer options</p>
-					<TimerOptions optionClick={this.props.onOptionClick} />
-					<BreakTimeOptions optionClick={this.props.onOptionClick} />
-					<button className='button-close' onClick={this.closeModal}>close</button>
-				</Modal>
+			<div className="wrapper-setting">
+				<span className="setting-icon" onClick={this.openModal}>
+					<i className="fas fa-cog" />
+				</span>
+				<Modal
+					isOpen={this.state.isModalOpen}
+					onCloseClick={this.closeModal}
+					optionClick={this.props.onOptionClick}
+				/>
 			</div>
 		);
 	}
@@ -36,12 +36,12 @@ class Setting extends React.Component {
 Setting.propTypes = {
 	onOptionClick: PropTypes.func,
 	onBreakOptionClick: PropTypes.func
-}
+};
 
 const modalStyle = {
 	color: '#000',
 	width: '35%',
-	height: '40%',
+	height: '45%',
 	position: 'absolute',
 	top: '50%',
 	left: '50%',
@@ -62,6 +62,10 @@ const backStyle = {
 };
 
 class Modal extends React.Component {
+	state = {
+		timerOpen: true
+	};
+
 	close = event => {
 		event.preventDefault();
 		if (this.props.onCloseClick) {
@@ -69,13 +73,32 @@ class Modal extends React.Component {
 		}
 	};
 
+	switchSetting = (event) => {
+		// this.setState({
+		// 	timerOpen:
+		// });
+	};
+
 	render() {
+		let content;
 		if (!this.props.isOpen) {
 			return null;
 		}
+		if (this.state.timerOpen) {
+			content = <TimerSetting optionClick={this.props.optionClick} closeModal={this.close}/>
+		} else {
+			content = <CategorySetting />
+		}
+
 		return (
 			<div>
-				<div style={modalStyle}>{this.props.children}</div>
+				<div style={modalStyle}>
+					<ul className='list-setting'>
+						<li onClick={this.switchSetting}>Timer</li>
+						<li onClick={this.switchSetting}>Category</li>
+					</ul>
+					{content}
+				</div>
 				<div style={backStyle} onClick={this.close} />
 			</div>
 		);
@@ -87,36 +110,45 @@ Modal.propTypes = {
 	onCloseClick: PropTypes.func
 };
 
+const TimerSetting = props => (
+	<div className='timer-setting'>
+		<h1>Timer Setting</h1>
+		<p>Choose your timer options</p>
+		<TimerOptions optionClick={props.optionClick} />
+		<BreakTimeOptions optionClick={props.optionClick} />
+		<button className="button-close" onClick={props.closeModal}>
+			Close
+		</button>
+	</div>
+);
+
+
 const TimerOptions = ({ optionClick }) => {
-  let options = [25, 45, 60];
-  let selectOption = event => {
-    optionClick(event.target.value);
-  };
+	const options = [25, 45, 60];
+	const selectOption = event => {
+		optionClick(event.target.value);
+	};
 
-  let TimerOptionButtons = options.map((option, i) => (
-  		<button
-  			key={i}
-  			className="button-modal button-timer"
-        onClick={selectOption}
-  			value={option}>
-  			{option} min
-  		</button>
-  	));
+	const TimerOptionButtons = options.map((option, i) => (
+		<button
+			key={i}
+			className="button-modal button-timer"
+			onClick={selectOption}
+			value={option}>
+			{option} min
+		</button>
+	));
 
-  return (
-    <div>
-    {TimerOptionButtons}
-    </div>
-  );
-}
+	return <div>{TimerOptionButtons}</div>;
+};
 
 TimerOptions.propTypes = {
 	optionClick: PropTypes.func
 };
 
 const BreakTimeOptions = ({ optionClick }) => {
-	let options = [10, 15, 20];
-	let selectOption = event => {
+	const options = [10, 15, 20];
+	const selectOption = event => {
 		optionClick(event.target.value);
 	};
 	const breakTimeOptionButtons = options.map((option, i) => (
@@ -128,15 +160,15 @@ const BreakTimeOptions = ({ optionClick }) => {
 			{option} min
 		</button>
 	));
-	return (
-    <div>
-    {breakTimeOptionButtons}
-    </div>
-  )
+	return <div>{breakTimeOptionButtons}</div>;
 };
 
 BreakTimeOptions.propTypes = {
 	optionClick: PropTypes.func
 };
+
+const CategorySetting = () => (<h1>Category Setting!</h1>)
+
+
 
 export default Setting;
