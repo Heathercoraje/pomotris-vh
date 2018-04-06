0cb530d0e6267a5936abba4c2e1b130c2655d38d
Vered Rekanati <veredrec@gmail.com>
2018-04-06 11:53:23 -0400

    Install D3 and react-faux-dom packages to render D3

--- src/components/Main/Recordboard.jsx
+++ src/components/Main/Recordboard.jsx
@@ -1,26 +1,36 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { formatTime } from '../../js/helper'
+import * as d3 from 'd3';
+import ReactFauxDOM from 'react-faux-dom';
+import { formatTime } from '../../js/helper';
 
 const Recordboard = ({ records }) => {
   const timeArray = records.map(record => Number(record.duration));
-  const totalTime = records.length ? formatTime(timeArray.reduce((a,b) => a + b) * 60 ) : null;
+  const totalTime = records.length
+    ? formatTime(timeArray.reduce((a, b) => a + b) * 60)
+    : null;
   const RecordList = records.map(
-		({ category, title, duration, startTime, id }, i) => (
-			<li className="recordItem" key={i} id={id}>
-				{[category, title, duration, startTime].join(' | ')}
-			</li>
+    ({ category, title, duration, startTime, id }, i) => (
+      <li className="recordItem" key={i} id={id}>
+        {[category, title, duration, startTime].join(' | ')}
+      </li>
     )
-	);
-	return (
-		<div className='RecordWrapper'>
-      <h2>{totalTime? 'Total' : ''} {totalTime}</h2>
-			<ul className="recordList">{RecordList}</ul>
-		</div>
-	);
+  );
+
+  return (
+    <div className="RecordWrapper">
+      <h2>
+        {totalTime ? 'Total' : ''} {totalTime}
+      </h2>
+      <ul className="recordList">{RecordList}</ul>
+    </div>
+  );
 };
 
 Recordboard.propTypes = {
-    records: PropTypes.array
-}
+  records: PropTypes.array
+};
+
 export default Recordboard;
+
+// D3 --------------------
