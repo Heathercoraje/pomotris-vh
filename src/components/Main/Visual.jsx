0b1a453a9b1feea2268590e5bb9044d6e4dcc173
Vered Rekanati <veredrec@gmail.com>
2018-04-08 17:24:00 -0400

    Add parts of the D3 vizualisation, still not rendering to page Relates #4

--- src/components/Main/Visual.jsx
+++ src/components/Main/Visual.jsx
@@ -3,57 +3,46 @@ import * as d3 from 'd3';
 import { withFauxDOM } from 'react-faux-dom';
 
 class Visual extends React.Component {
-	// static propTypes = {...}
+  // static propTypes = {...}
 
-	componentDidMount() {
-		const visual = this.props.connectFauxDOM('div', 'chart');
-		// D3 Code to create the shape
-		// using faux as container
-		const shape = d3
-			.select(visual)
-			.append('svg')
-			.attr('width', 400)
-			.attr('height', 400)
-			.attr('stroke-width', 2)
-			.attr('stroke', 'grey');
-		shape
-			.append('rect')
-			.attr('width', '50')
-			.attr('height', '50')
-			.attr('x', '100')
-			.attr('y', '60')
-			.text('H')
-			.style('fill', 'orange');
-		shape
-			.append('rect')
-			.attr('width', '50')
-			.attr('height', '50')
-			.attr('x', '200')
-			.attr('y', '60')
-			.text('H')
-			.style('fill', 'pink');
-		shape
-			.append('rect')
-			.attr('width', '50')
-			.attr('height', '50')
-			.attr('x', '300')
-			.attr('y', '60')
-			.text('H')
-			.style('fill', 'green');
-	}
+  componentDidMount() {
+    // createBox() {
+    const visual = this.props.connectFauxDOM('div', 'chart');
+    // D3 Code to create the shape
+    // using faux as container
+    console.log(this.props.categories);
+    console.log(this.props.records);
+
+    d3
+      .select('visual')
+      .selectAll('div')
+      .data(categories) // bind our data
+      .enter()
+      .append('div')
+      // .selectAll('div') // inner selection
+      // .data(d => d3.range(d.time / 5).map(() => d))
+      // .enter()
+      .text(d => d.category)
+      .attr('class', 'block')
+      .style('background-color', d => d.color);
+  }
   componentDidUpdate() {
     // this is the place where you get data being fetched from localStorage or from parent.
     // componentDidMount only does initial render
     const records = this.props.records;
     const categories = this.props.categories;
+
     console.log('after update');
     console.log('Records : ', records);
     console.log('Categories :', categories);
+    // return visual.toReact();
   }
-  
-	render() {
-		return <div className="line-container">{this.props.chart}</div>;
-	}
+  // }
+  render() {
+    // return <div className="line-container">{this.createBox()}</div>;
+
+    return <div className="line-container">{this.props.chart}</div>;
+  }
 }
 
 export default withFauxDOM(Visual);
