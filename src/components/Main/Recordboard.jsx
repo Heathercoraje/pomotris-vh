17370871b37199c87c6277adbb1a004310122fc6
heathercoraje <heathercoraje@gmail.com>
2018-04-17 14:01:22 +0900

    toggle button to change display mode relates #51

--- src/components/Main/Recordboard.jsx
+++ src/components/Main/Recordboard.jsx
@@ -1,35 +1,72 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 // import * as d3 from 'd3';
 // import ReactFauxDOM from 'react-faux-dom';
 import { formatTime } from '../../js/helper';
 import Visual from './Visual';
 
-const Recordboard = ({ records, categories }) => {
-  const timeArray = records.map(record => Number(record.duration));
-  const totalTime = records.length
-    ? formatTime(timeArray.reduce((a, b) => a + b) * 60)
-    : null;
-  const RecordList = records.map(
-    ({ category, task, duration, startTime, id }, i) => (
-      <li className="recordItem" key={i} id={id}>
-        {[category, task, duration, startTime].join(' | ')}
-      </li>
-    )
-  );
+class Recordboard extends React.Component {
+	state = {
+		cubeMode: true
+	};
 
-  return (
-    <div className="RecordWrapper">
-      <h2>
-        {totalTime ? 'Total' : ''} {totalTime}
-      </h2>
-      <Visual records={records} categories={categories}/>
-    </div>
-  );
-};
+	GetTotalTime = records => {
+		const timeArray = records.map(record => Number(record.duration));
+		const totalTime = records.length
+			? formatTime(timeArray.reduce((a, b) => a + b) * 60)
+			: null;
+		return totalTime;
+	};
+	toggleDisplayMode = event => {
+		const temp = this.state.cubeMode;
+		this.setState({
+			cubeMode: !temp
+		});
+	};
+
+	toggleButtonText = () => {
+		const isCubeMode = this.state.cubeMode;
+		const buttonText = isCubeMode ? 'Show list' : 'Show cubes';
+		return buttonText;
+	};
+
+	render() {
+		const totalTime = this.GetTotalTime(this.props.records);
+		const { records, categories } = this.props;
+		const RecordList = records.map(
+			({ category, task, duration, startTime, id }, i) => (
+				<li className="recordItem" key={i} id={id}>
+					{[category, task, duration, startTime].join(' | ')}
+				</li>
+			)
+		);
+		let content;
+		if (this.state.cubeMode) {
+			content = <Visual records={records} categories={categories} />;
+		} else {
+			content = RecordList;
+		}
+		return (
+			<div className="RecordWrapper">
+				<div className="recordHelper">
+					<button
+						className="toggleButton helerItem "
+						onClick={this.toggleDisplayMode}
+					>
+						{this.toggleButtonText()}
+					</button>
+					<span className="totalTimeContainer helerItem ">
+						{totalTime ? 'Total' : ''} {totalTime}
+					</span>
+				</div>
+				{content}
+			</div>
+		);
+	}
+}
 
 Recordboard.propTypes = {
-  records: PropTypes.array
+	records: PropTypes.array
 };
 
 export default Recordboard;
