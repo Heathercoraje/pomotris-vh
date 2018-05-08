f957ceca0cef42f05e147b5a74b9b0565a09714a
heathercoraje <heathercoraje@gmail.com>
2018-05-08 11:42:57 +0900

    fix profile card font and height

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => (
 		<p className="footer-text">
 			{' '}
 			Pomotris version 1.0. 2018{' '}
-			<a target="_blank" href="www.github/pomotris-vh">
+			<a target="_blank" href="https://github.com/Heathercoraje/pomotris-vh">
 				<i className="fab fa-github" />
 			</a>
 		</p>
